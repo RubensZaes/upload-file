f16c1a972db02b373858d28c048ab76027834601
RubensZaes <rubenszaes@gmail.com>
2023-10-09 16:42:14 -0300

    :tada: init Commit Inicial

---
+++ src/main/java/br/com/rubenszaes/uploadfile/UploadFileComSpringooobtApplication.java
@@ -0,0 +1,13 @@
+package br.com.rubenszaes.uploadfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadFileComSpringooobtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadFileComSpringooobtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rubenszaes/uploadfile/UploadFileComSpringooobtApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rubenszaes.uploadfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadFileComSpringooobtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
