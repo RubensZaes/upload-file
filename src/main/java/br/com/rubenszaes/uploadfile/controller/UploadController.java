0d5aafa086a299c2148f11e09decaad3b3cf736e
RubensZaes <rubenszaes@gmail.com>
2023-10-09 17:13:55 -0300

    :sparkles: feat Controller para Upload de Arquivo.

--- src/main/java/br/com/rubenszaes/uploadfile/controller/UploadController.java
+++ src/main/java/br/com/rubenszaes/uploadfile/controller/UploadController.java
@@ -1,2 +1,35 @@
-package br.com.rubenszaes.uploadfile.controller;public class UploadController {
+package br.com.rubenszaes.uploadfile.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Controller
+public class UploadController {
+
+    private Path pathRaiz = Paths.get("./uploads");
+
+    @GetMapping("/")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("Index");
+        return modelAndView;
+    }
+    @PostMapping("/upload")
+    public ModelAndView uploadFile(MultipartFile multipartFile) {
+        try {
+            Files.createDirectories(pathRaiz);
+            Files.copy(multipartFile.getInputStream(), pathRaiz.resolve(multipartFile.getOriginalFilename()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new ModelAndView("redirect:/");
+    }
+
 }
